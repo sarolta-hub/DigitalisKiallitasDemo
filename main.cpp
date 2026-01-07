45a454bd878013f3ff7a301c2d96f7e1104564d6
SaroltaMihaly <mihaly.sarolta01@gmail.com>
2026-01-07 11:19:55 +0200

    Added initial files

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "message_lib.h"
+
+// Nev:
+// Csoport:
+
+int main() {
+    std::cout << get_message() << std::endl;
+    return 0;
+}
---
+++ message_lib.h
@@ -0,0 +1,10 @@
+#ifndef MESSAGE_LIB_H
+#define MESSAGE_LIB_H
+
+#include <string>
+
+inline std::string get_message() {
+    // teritse vissza ezt a szoveget <Digitalis Kiallitas Demo>
+}
+
+#endif
